8a35668573653f2e3a3d6fcb402b865269a0501b
grandiosa iva <ggo24@yahoo.com>
2021-08-09 19:40:40 +0400

    Before Db modifications

--- Distribution/Areas/User/Controllers/RecordController.cs
+++ Distribution/Areas/User/Controllers/RecordController.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Distribution.Data;
 using Distribution.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Distribution.Areas.User.Controllers
 {
     [Area("User")]
+    [Authorize(Roles = "Admin, User")]
     public class RecordController : Controller
     {
         private readonly ApplicationDbContext _context;
